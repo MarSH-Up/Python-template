2fdf715abc9adcb8d5445cbea2a6b36e2684b034
MarSHub <madlsh@icloud.com>
2023-09-10 14:53:02 -0600

    [Initial Commit] Setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import os
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+with open('requirements.txt', 'r', encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='your_project_name',
+    version='0.1.0',
+    description='fNIRS Synthetic data generator based on the Bilinear Model [S.Tak 2015] ',
+    long_description_content_type="text/markdown",
+    author='Mario De Los Santos',
+    author_email='madlsh3517@gmail.com',
+    url='https://github.com/yourusername/your_project_name',
+    install_requires=requirements,
+    packages=find_packages(exclude=('tests*', 'docs')),
+    license='MIT', 
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.11',
+    ],
+    python_requires='>=3.8', 
+)
+
